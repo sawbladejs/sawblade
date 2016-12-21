147c69020871cf2c38d5714a0a45633dc91350b5
Nick Saunders <nick@saunde.rs>
2016-12-21 09:40:51 -0700

    New API similar to vue-router, angular router, etc.

--- src/watch.js
+++ src/watch.js
@@ -1,28 +1,92 @@
-export default function watch(url) {
-  return function route(path, callback) {
-    let currentUrl = null;
-    let currentParams = null;
+export default function watch(handleUrlChanges) {
+  let root;
+  let config;
 
-    url(newUrl => {
-      const newParams = newUrl
-        .substring(path.length)
-        .split(';')
-        .filter(param => param)
-        .map(param => param.split('='))
-        .map(param => ({ [param[0]]: isNaN(param[1]) ? param[1] : parseInt(param[1]) }))
-        .reduce((params, param) => Object.assign({}, params, param), {});
+  let previousRouteInstances = [];
 
-      const paramsChanged = JSON.stringify(newParams) !== JSON.stringify(currentParams);
+  handleUrlChanges(next => {
+    const nextRouteInstances = createRouteInstances(next, config);
 
-      if (newUrl.startsWith(path) && (currentUrl === null || !currentUrl.startsWith(path) || paramsChanged)) {
-        callback({
-          route: (subpath, callback) => route(path + subpath, callback),
-          params: newParams
-        });
-      }
+    previousRouteInstances
+      .filter(previous => !nextRouteInstances.find(next => equalRouteInstances(previous, next)))
+      .reverse()
+      .forEach(instance => instance.config.teardown(instance.context));
 
-      currentUrl = newUrl;
-      currentParams = newParams;
-    });
+    nextRouteInstances
+      .map(next => ({
+        previous: previousRouteInstances.find(previous => equalRouteInstances(previous, next)),
+        next
+      }))
+      .filter(({ previous }) => previous)
+      .forEach(({ previous, next }) => next.context = previous.context);
+
+    nextRouteInstances
+      .filter(instance => !instance.context)
+      .map(instance => ({ instance, position: nextRouteInstances.indexOf(instance) }))
+      .forEach(({ instance, position }) => instance.context = instance.config.render({
+        parent: position < 1 ? root : nextRouteInstances[position - 1].context,
+        params: getParams(instance.match, instance.config.path)
+      }));
+
+    previousRouteInstances = nextRouteInstances;
+  });
+
+  function createRouteInstances(url, config) {
+    const match = url.match(/(\/[^/]*)/)[0];
+    const matchConfig = config.find(config => configMatchesPath(config, match));
+    const instance = createRouteInstance(match, matchConfig);
+    const descendantUrl = url.substring(match.length);
+
+    if (descendantUrl && matchConfig.children) {
+      return [instance].concat(createRouteInstances(descendantUrl, matchConfig.children));
+    }
+
+    return [instance];
+  }
+
+  function createRouteInstance(match, config) {
+    return { config, match };
+  }
+
+  function equalRouteInstances(instance1, instance2) {
+    return instance1.config === instance2.config && instance1.match === instance2.match;
+  }
+
+  function configMatchesPath(config, path) {
+    const testPath = path.split(';')[0];
+
+    if (config.path === testPath) {
+      return true;
+    }
+
+    if (config.path.startsWith('/:') && /^\/.+$/.test(testPath)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function getParams(match, configPath) {
+    const matchParts = match.split(';');
+
+    const params = matchParts.slice(1)
+      .map(param => param.split('='))
+      .map(param => ({ [param[0]]: normalizeParamValue(param[1]) }))
+      .reduce((params, param) => Object.assign({}, params, param), {});
+
+    if (configPath.startsWith('/:')) {
+      params[configPath.substring(2)] = normalizeParamValue(matchParts[0].substring(1));
+    }
+
+    return params;
+  }
+
+  function normalizeParamValue(value) {
+    return isNaN(value) ? value : parseInt(value);
+  }
+
+  return function route(r, c) {
+    root = r;
+    config = c;
   };
 }
--- src/watch.spec.js
+++ src/watch.spec.js
@@ -1,100 +1,185 @@
-import { spy } from 'sinon';
 import { expect } from 'chai';
+import { stub, spy, match } from 'sinon';
 import watch from './watch';
 
-describe('watch', () => {
-  describe('given a URL provider', () => {
-    let url;
-    let route;
+describe('watch (configured with a URL provider)', () => {
+  const rootContext = 'root context';
 
-    function changeUrl(newUrl) {
-      for (let i = 0; i < url.callCount; i++) {
-        url.getCall(i).args[0](newUrl);
-      }
-    }
+  let route;
+  let changeUrl;
+
+  beforeEach(() => route = watch(callback => changeUrl = callback));
+
+  describe('when configured with two root-level routes', () => {
+    const defaultRender = stub();
+    const defaultContext = 'default context';
+    defaultRender.returns(defaultContext);
+    const defaultTeardown = spy();
+
+    const usersRender = stub();
+    const usersContext = 'users context';
+    usersRender.returns(usersContext);
+    const usersTeardown = spy();
 
     beforeEach(() => {
-      url = spy();
-      route = watch(url);
+      route(rootContext, [
+        {
+          path: '/',
+          render: defaultRender,
+          teardown: defaultTeardown
+        },
+        {
+          path: '/users',
+          render: usersRender,
+          teardown: usersTeardown
+        }
+      ]);
     });
 
-    describe('when configured only with empty root route', () => {
-      const callback = spy();
+    afterEach(() => [defaultRender, defaultTeardown, usersRender, usersTeardown].forEach(spy => spy.reset()));
 
-      beforeEach(() => route('', callback));
+    describe('when the URL changes to /', () => {
+      beforeEach(() => changeUrl('/'));
 
-      afterEach(() => callback.reset());
+      it('should activate the default route with root context', () => {
+        expect(defaultRender).to.have.been.calledWith(match.has('parent', rootContext));
+      });
 
-      describe('when the URL changes to /hello', () => {
-        beforeEach(() => changeUrl('/hello'));
+      it('should not activate the users route', () => {
+        expect(usersRender).not.to.have.been.called;
+      });
 
-        it('should invoke the callback', () => {
-          expect(callback).to.have.been.called;
-        });
+      describe('and then changes to /users', () => {
+        beforeEach(() => changeUrl('/users'));
 
-        describe('and then changes to /goodbye', () => {
-          beforeEach(() => changeUrl('/goodbye'));
-
-          it('should not invoke the callback a second time', () => {
-            expect(callback.callCount).to.be.lessThan(2);
-          });
+        it('should tear down the default route, providing the context', () => {
+          expect(defaultTeardown).to.have.been.calledWith(defaultContext);
         });
       });
     });
 
-    describe('when configured with empty root route and sub-routes /posts and /users', () => {
-      const postsCallback = spy();
-      const usersCallback = spy();
+    describe('when the URL changes to /users', () => {
+      beforeEach(() => changeUrl('/users'));
 
-      beforeEach(() => {
-        route('', ({ route }) => {
-          route('/posts', postsCallback);
-          route('/users', usersCallback);
-        });
+      it('should activate the users route with root context', () => {
+        expect(usersRender).to.have.been.calledWith(match.has('parent', rootContext));
       });
 
-      afterEach(() => [postsCallback, usersCallback].forEach(callback => callback.reset()));
-
-      describe('when the URL changes to /posts', () => {
-        beforeEach(() => changeUrl('/posts'));
-
-        it('should invoke the posts callback', () => {
-          expect(postsCallback).to.have.been.called;
-        });
+      it('should not activate the default route', () => {
+        expect(defaultRender).not.to.have.been.called;
       });
 
-      describe('when the URL changes to /users;page=3;sort=asc', () => {
-        beforeEach(() => changeUrl('/users;page=3;sort=asc'));
+      describe('and then changes to /', () => {
+        beforeEach(() => changeUrl('/'));
 
-        it('should invoke the users callback', () => {
-          expect(usersCallback).to.have.been.called;
+        it('should tear down the users route, providing the context', () => {
+          expect(usersTeardown).to.have.been.calledWith(usersContext);
         });
+      });
+    });
 
-        it('should pass parameters to callback', () => {
-          let params = null;
-          if (usersCallback.called) {
-            params = usersCallback.getCall(0).args[0].params;
-          }
-          expect(params).to.deep.equal({ page: 3, sort: 'asc' });
+    describe('when the URL changes to /users;x=y;page=3', () => {
+      beforeEach(() => changeUrl('/users;x=y;page=3'));
+
+      it('should provide the matrix parameters to the render function', () => {
+        expect(usersRender).to.have.been.calledWith(match.has('params', match({ x: 'y', page: 3 })));
+      });
+
+      describe('and then changes to /users;x=y;page=4', () => {
+        beforeEach(() => changeUrl('/users;x=y;page=4'));
+
+        it('should tear down the users route and re-activate with new params', () => {
+          expect(usersTeardown).to.have.been.called;
+          expect(usersRender).to.have.been.calledWith(match.has('params', match({ page: 4 })))
         });
+      });
+    });
+  });
 
-        describe('when the URL changes to /users;page=4;sort=asc', () => {
-          beforeEach(() => {
-            usersCallback.reset();
-            changeUrl('/users;page=4;sort=asc');
-          });
+  describe('when configured with a route with a named parameter', () => {
+    const render = spy();
 
-          it('should invoke the users callback', () => {
-            expect(usersCallback).to.have.been.called;
-          });
+    beforeEach(() => {
+      route(rootContext, [
+        {
+          path: '/:id',
+          render
+        }
+      ]);
+    });
 
-          it('should pass parameters to callback', () => {
-            let params = null;
-            if (usersCallback.called) {
-              params = usersCallback.getCall(1).args[0].params;
+    afterEach(() => render.reset());
+
+    describe('when the URL changes to /blah', () => {
+      beforeEach(() => changeUrl('/blah'));
+
+      it('should provide the id parameter value "blah" to the render method', () => {
+        expect(render).to.have.been.calledWith(match.has('params', { id: 'blah' }));
+      });
+    });
+  });
+
+  describe('when configured with child routes', () => {
+    const usersRender = stub();
+    const usersContext = 'users context';
+    usersRender.returns(usersContext);
+    const usersTeardown = spy();
+
+    const listRender = stub();
+    const listContext = 'list context';
+    listRender.returns(listContext);
+    const listTeardown = spy();
+
+    const detailRender = spy();
+
+    beforeEach(() => {
+      route(rootContext, [
+        {
+          path: '/users',
+          render: usersRender,
+          teardown: usersTeardown,
+          children: [
+            {
+              path: '/list',
+              render: listRender,
+              teardown: listTeardown
+            },
+            {
+              path: '/:id',
+              render: detailRender,
+              teardown: () => {}
             }
-            expect(params).to.deep.equal({ page: 4, sort: 'asc' });
-          });
+          ]
+        }
+      ]);
+    });
+
+    afterEach(() => [usersRender, usersTeardown, listRender, listTeardown, detailRender].forEach(spy => spy.reset()));
+
+    describe('when the URL changes to /users/list', () => {
+      beforeEach(() => changeUrl('/users/list'));
+
+      it('should activate the users route', () => {
+        expect(usersRender).to.have.been.called;
+      });
+
+      it('should activate the list route with the users context', () => {
+        expect(listRender).to.have.been.calledWith(match.has('parent', usersContext));
+      });
+
+      describe('and then the URL changes to /users/123', () => {
+        beforeEach(() => changeUrl('/users/123'));
+
+        it('should tear down the list route', () => {
+          expect(listTeardown).to.have.been.called;
+        });
+
+        it('should not tear down the users route', () => {
+          expect(usersTeardown).not.to.have.been.called;
+        });
+
+        it('should activate the detail route', () => {
+          expect(detailRender).to.have.been.calledWith(match.has('params', match({ id: 123 })));
         });
       });
     });
